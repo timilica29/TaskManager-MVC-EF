53cdcfc5f7715f1648038ca1362bef6eea786e29
Timi Gubernu <dtimigubernu@gmail.com>
2025-05-23 15:00:41 +0300

    Added Task model and TaskStatus enum

---
+++ Models/Enums/Status.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskManager.Models.Enums
+{
+    public enum Status
+    {
+        ToDo,
+        InProgress,
+        Done
+    }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TaskManager.Models.Enums;
+
+namespace TaskManager.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DueDate { get; set; }
+
+        public Status TaskStatus { get; set; }
+    }
+}
